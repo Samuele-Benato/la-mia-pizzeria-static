07b7e357030af0547e519dd7375d6831c51112e1
Samuele-Benato <samuele.benato@gmail.com>
2024-05-10 14:42:21 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Pizzeria.cs
@@ -0,0 +1,13 @@
+﻿namespace la_mia_pizzeria_static
+{
+    public class Pizzeria
+    {
+        public string Name { get; set; }
+        public List<Pizza> pizzas { get; set; }
+
+        public Pizzeria(string name) 
+        {
+            Name = name;
+        }
+    }
+}
